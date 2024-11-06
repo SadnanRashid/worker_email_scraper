9b3745f200af4d02f2750422e44fd6d18569403f
SadnanRashid <sadnan.rashid.07@gmail.com>
2024-11-06 19:53:48 +0600

    update

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import { createPuppeteerRouter } from "crawlee";
+
+export const router = createPuppeteerRouter();
+
+router.addDefaultHandler(async ({ page, request, log }) => {
+    log.info(`Extracting HTML content for ${request.url}`);
+
+    // Extract the HTML content of the page
+    const htmlContent = await page.content();
+
+    log.info(`HTML extracted and saved for ${request.url}`);
+
+    console.log(htmlContent);
+});
