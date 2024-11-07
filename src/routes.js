0e18779df97a1c4d8ddab0a62e736cadaf4f42a2
SadnanRashid <sadnan.rashid.07@gmail.com>
2024-11-07 15:58:25 +0600

    update backup

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { createPuppeteerRouter } from "crawlee";
-import { getAllLinks } from "./parser.js";
+import { getAllLinks, extractDataFromHtml } from "./parser.js";
 
 export const router = createPuppeteerRouter();
 
 router.addDefaultHandler(async ({ page, request, log }) => {
+    const arrayOfData = [];
     log.info(`Extracting HTML content for ${request.url}`);
 
     // Extract the HTML content of the page
@@ -11,6 +12,8 @@ router.addDefaultHandler(async ({ page, request, log }) => {
 
     // Get all possible links that might contain the data (email/socials)
     const links = getAllLinks(htmlContent, request.url);
+
+    // Here I want to scrape all links (links array) and use extractDataFromHtml to get the emails
 
     log.info(`HTML extracted and saved for ${request.url}`);
 
